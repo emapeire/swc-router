1f5d22bcb2c11aa7c9c9bb5898cb1af207f122b6
Emanuel Peire <emapeire@outlook.com>
2023-11-20 17:23:22 -0300

    Refactor Link component to handle click events more efficiently

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,8 +3,15 @@ import navigate from '../lib/navigate'
 
 export default function Link({ target, to, ...props }) {
   const handleClick = (event) => {
-    event.preventDefault()
-    navigate(to)
+    const isMainEvent = event.button === 0
+    const isModifierEvent =
+      event.metaKey || event.altKey || event.ctrlKey || event.shiftKey
+    const isManageableEvent = target === undefined || target === '_self'
+
+    if (isMainEvent && isManageableEvent && !isModifierEvent) {
+      event.preventDefault()
+      navigate(to)
+    }
   }
 
   return <a onClick={handleClick} href={to} target={target} {...props} />
