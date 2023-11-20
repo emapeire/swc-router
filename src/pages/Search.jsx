a5c911fda1b81a8b03cb86835be6b37a804162cc
Emanuel Peire <emapeire@outlook.com>
2023-11-20 18:49:58 -0300

    Update document title based on search query

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,11 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from 'react'
+
 export default function SearchPage({ routeParams }) {
+  useEffect(() => {
+    document.title = `${routeParams.query}`
+  }, [routeParams])
+
   return (
     <h1>
       You have searched{' '}
