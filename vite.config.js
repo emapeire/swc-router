9e332f43bde1b72cf4f2e07db49944a50e4996d0
Emanuel Peire <emapeire@outlook.com>
2023-11-21 17:56:55 -0300

    added configuration for build settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [react()],
   test: {
     environment: 'happy-dom'
